e50da97dcb30390450672bc0bf5fd4f9cba1e556
Roslyn Saplicki <j.saplicki@gmail.com>
2018-12-18 14:41:05 -0800

    NavBar added

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = (props) => {
+    <div>
+        <div>HOME</div>
+        <div>MUSIC</div>
+        <div>MERCH</div>
+        <div>EVENTS</div>
+    </div>
+}
+
+
+export default NavBar;
