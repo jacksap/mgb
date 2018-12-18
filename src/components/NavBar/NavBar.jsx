4620a7b2521b02841c6d2e3b32431178cb971931
Roslyn Saplicki <j.saplicki@gmail.com>
2018-12-18 14:58:18 -0800

    NavBar displayed

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './NavBar.css';
 
 const NavBar = (props) => {
+    return(
     <div>
         <div>HOME</div>
         <div>MUSIC</div>
         <div>MERCH</div>
         <div>EVENTS</div>
     </div>
+    )
 }
 
 
