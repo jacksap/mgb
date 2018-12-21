0d82d9d65e6487841287b11368cc77aeac97e046
Roslyn Saplicki <j.saplicki@gmail.com>
2018-12-20 19:28:34 -0500

    Merch component

---
+++ src/components/Merch/Merch.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Merch.css';
+
+const Merch = (props) => {
+    return(
+    <div>
+        <div>Merch COMPONENT</div>
+    </div>
+    );
+}
+
+
+export default Merch;
