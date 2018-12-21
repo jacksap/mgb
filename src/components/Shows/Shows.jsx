1a66a8f71be8cb598d81ff41b8abef7c16f25225
Roslyn Saplicki <j.saplicki@gmail.com>
2018-12-20 19:27:14 -0500

    Saving Shows

---
+++ src/components/Shows/Shows.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Shows.css';
+
+const Shows = (props) => {
+    return(
+    <div>
+        <div>SHOWS COMPONENT</div>
+    </div>
+    );
+}
+
+
+export default Shows;
