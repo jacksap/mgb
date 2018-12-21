fae1de67c880b682747bd81cdb6d9d99acbc00a0
Roslyn Saplicki <j.saplicki@gmail.com>
2018-12-20 19:26:50 -0500

    Saving Component

---
+++ src/components/Gallery/Gallery.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Gallery.css';
+
+const Gallery = (props) => {
+    return(
+    <div>
+        <div>Gallery COMPONENT</div>
+    </div>
+    );
+}
+
+
+export default Gallery;
---
+++ src/components/Music/Music.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Music.css';
+
+const Music = (props) => {
+    return(
+    <div>
+        <div>Music COMPONENT</div>
+    </div>
+    );
+}
+
+
+export default Music;
