edb4b23e21ea2f6f5392d23e9e1c91822c5bb0d3
Roslyn Saplicki <j.saplicki@gmail.com>
2018-12-22 16:52:36 -0500

    imports contact component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import Contact from './components/Contact/Contact';
 
 class App extends Component {
   render() {
@@ -8,8 +9,7 @@ class App extends Component {
       <div className="App">
         <NavBar />
         <footer>
-          Contact Us
-          {/* Should this be a component, if I am asking it probably should be. */}
+          <Contact />
         </footer>
       </div>
     );
