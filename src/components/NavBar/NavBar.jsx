40efdcfef49053f3600a221dd61284656dc20d2d
Roslyn Saplicki <j.saplicki@gmail.com>
2018-12-22 16:41:16 -0500

    Router paths added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import './NavBar.css';
 
+import Music from '../Music/Music'
+import Shows from '../Shows/Shows'
+import Merch from '../Merch/Merch'
+import Gallery from '../Gallery/Gallery'
+
 const NavBar = (props) => {
     return(
-    <div>
-        {/* REACT ROUTER */}
-        <div>HOME</div>
-        <div>MUSIC</div>
-        <div>MERCH</div>
-        <div>SHOWS</div>
-        <div>GALLERY</div>
-    </div>
+        <Router>
+        <div className="NavBar">
+        <nav>
+          <Link to="/">HOME</Link>{' '}
+          <Link to="/music">MUSIC</Link>
+          <Link to="/shows">SHOWS</Link>
+          <Link to="/merch">MERCH</Link>{' '}
+          <Link to="/gallery">GALLERY</Link>{' '}
+        </nav>
+          <Route exact path='/' component={Home} />
+          <Route path='/music' render={Music} />
+          <Route path='/shows' render={Shows} />
+          <Route path='/merch' render={Merch} />
+          <Route path='/gallery' component={Gallery} />
+        </div>
+      </Router>
     );
 }
 
