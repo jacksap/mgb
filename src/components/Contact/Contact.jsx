0df2e588969c3e9b021b1b0f64b0b3733f951795
Roslyn Saplicki <j.saplicki@gmail.com>
2018-12-22 16:51:36 -0500

    Contact component created

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Contact.css';
+
+const Contact = (props) => {
+    return(
+    <div>
+        <div>Contact Us</div>
+    </div>
+    );
+}
+
+
+export default Contact;
