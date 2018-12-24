58f20c7402d448fef5789854e7ee29b996309b49
Roslyn Saplicki <j.saplicki@gmail.com>
2018-12-24 15:29:27 -0500

    headder

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar />
+        <header>
+          <NavBar />
+        </header>
         <footer>
           <Contact />
         </footer>
