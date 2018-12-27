6f6d01bd7b99c9ee298f9c153d103c7fe3511ff7
Roslyn Saplicki <j.saplicki@gmail.com>
2018-12-27 10:41:42 -0500

    testing the component styles

--- src/components/Merch/Merch.jsx
+++ src/components/Merch/Merch.jsx
@@ -5,6 +5,12 @@ const Merch = (props) => {
     return(
     <div>
         <div>Merch COMPONENT</div>
+        <ul>
+            <li>Shirt</li>
+            <li>EP</li>
+            <li>LP</li>
+            <li>Sticker</li>
+        </ul>
     </div>
     );
 }
